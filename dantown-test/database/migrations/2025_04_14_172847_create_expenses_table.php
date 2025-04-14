6c476c8bcbd0f92968bb50bf80631ab60ebf5923
Abayomi Akinfemiwa <olayodepossible@gmail.com>
2025-04-14 18:55:52 +0100

    complete database model and migration

--- dantown-test/database/migrations/2025_04_14_172847_create_expenses_table.php
+++ dantown-test/database/migrations/2025_04_14_172847_create_expenses_table.php
@@ -13,13 +13,14 @@ return new class extends Migration
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('company_id')->constrained()->onDelete('cascade')->index();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade')->index();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('company_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->decimal('amount', 10, 2);
             $table->string('category');
             $table->timestamps();
-            $table->timestamps();
+
+            $table->index(['company_id', 'user_id']);
         });
     }
 
