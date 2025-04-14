84d117d9d792a762cfadedc25edfb20937ccef60
Dauda Pona <daudapona2@gmail.com>
2025-04-14 14:09:20 +0100

    chore: code cleanup

--- routes/Api/V1/Admin/UserManagementRoutes.php
+++ routes/Api/V1/Admin/UserManagementRoutes.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\V1\Admin\UserManagement\FetchUsersController;
 use App\Http\Controllers\V1\Admin\UserManagement\UpdateUserController;
 use Illuminate\Support\Facades\Route;
 
-Route::group([], function() {
+Route::group([], function () {
     Route::post('/users', CreateUserController::class);
     Route::get('/users', FetchUsersController::class);
     Route::put('/users/{userId}', UpdateUserController::class);
