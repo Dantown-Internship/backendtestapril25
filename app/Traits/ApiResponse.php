87f347d41b4c9c82befed81baabc8256afe0f354
oyenet1 <bowofadeoyerinde@gmail.com>
2025-04-15 09:41:27 +0100

    company global scope added

---
+++ app/Traits/ApiResponse.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponse
+{
+    public static function success($data, $message = "Success", $statusCode = 200)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data
+        ], $statusCode);
+    }
+
+    public static function failure($message = "failure", $statusCode = 400)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+        ], $statusCode);
+    }
+}
