968304e1e80f7f60e4631a30aa270cc1049027dc
oyenet1 <bowofadeoyerinde@gmail.com>
2025-04-15 14:07:44 +0100

    seeders errors resolved

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -2,16 +2,29 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CompanySeeder extends Seeder
 {
+    protected $model = Company::class;
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        // default users for testing I omitted register api
+        $companies = Company::factory()->count(2)->make();
+
+        foreach ($companies as $key => $company) {
+            $company->users()->create([
+                'name' => fake()->name(),
+                'email' => "admin@company{$key}.com",
+                'password' => Hash::make('password'),
+                'role' => 'Admin',
+            ]);
+        }
     }
 }
