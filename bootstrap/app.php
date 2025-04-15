c80740cdb2c4af156ac2a5ef35b5a299ca3a034a
oyenet1 <bowofadeoyerinde@gmail.com>
2025-04-15 10:36:57 +0100

    route file setup for better api versioning

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,29 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: [
+            __DIR__ . '/../routes/api.php',
+            __DIR__ . '/../routes/api/v1.php', // Add this line
+        ],
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->map(function (NotFoundHttpException $exception, $request) {
+            if (str_starts_with($request->getPathInfo(), '/api/')) {
+                return response()->json([
+                    'error' => 'Resource not found'
+                ], 404);
+            }
+
+            return response()->view('errors.404', [], 404);
+        });
     })->create();
