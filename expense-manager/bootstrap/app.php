44ed50d21e5081a1ed5c5322865d3058f9b1fc38
Ade-ojo Toluwanimi <59847687+ManLikeTeetos@users.noreply.github.com>
2025-04-15 22:18:13 +0000

    Model and SetTenantContext

--- expense-manager/bootstrap/app.php
+++ expense-manager/bootstrap/app.php
@@ -11,8 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        
         $middleware->append(
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class
+        );
+
+        $middleware->append(
+            \App\Http\Middleware\SetTenantContext::class
         );
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- expense-manager/database/migrations/0001_01_01_000000_create_users_table.php
+++ expense-manager/database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('company_id')->constrained()->onDelete('cascade');
+            $table->enum('role', ['Admin', 'Manager', 'Employee']);
             $table->timestamps();
         });
 
