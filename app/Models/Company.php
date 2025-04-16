f9695db1d32fc579253cc05ce2bc3dd65b47c71f
doobie-droid <lesliedouglas23@gmail.com>
2025-04-16 22:50:21 +0100

    create Company Model and add annotations for intellisense create Company Factory create Company Seeder and use in Database seeder to fill db with fake data create migration for companies table

---
+++ app/Models/Company.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Company
+ *
+ * @property int id
+ * @property string name
+ * @property string email
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
+class Company extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'email',
+    ];
+
+    /** @use HasFactory<\Database\Factories\CompanyFactory> */
+    use HasFactory;
+}
