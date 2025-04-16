f98585e81d0168b4bc4f341925ec64e10fc74220
Favour Oladeji <oladejifavour100@gmail.com>
2025-04-16 13:29:10 +0100

    feat: implement login and logout endpoints

---
+++ app/Http/Controllers/Api/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): JsonResponse
+    {
+        $request->authenticate();
+        $user = auth()->user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return $this->successResponse("Login successful", [
+            'user' => new UserResource($user->load('company')),
+            'token' => [
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'expires_in' => config('sanctum.expiration') ? config('sanctum.expiration') * 60 : null,
+            ],
+        ]);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->successResponse('Logout successful');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\ExpenseController;
+use App\Http\Controllers\Api\Auth\AuthenticatedSessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::prefix('/v1')->group(function () {
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    ->middleware('guest')
+    ->name('login');
+
+    Route::middleware('auth:sanctum')->group(function  () {
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+            ->name('logout');
+    });
+});
+
+
+
+// Route::get('/expenses', [ExpenseController::class, 'index'])->name('expenses.index');
+// Route::post('/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
+// Route::get('/expenses/{expense}', [ExpenseController::class, 'show'])->name('expenses.show');
+// Route::put('/expenses/{expense}', [ExpenseController::class, 'update'])->name('expenses.update');
+// Route::delete('/expenses/{expense}', [ExpenseController::class, 'destroy'])->name('expenses.destroy');
