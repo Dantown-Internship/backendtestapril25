4bf4b48ac7b2b5fe58c964e9476585fec8058be3
Favour Oladeji <oladejifavour100@gmail.com>
2025-04-16 10:39:24 +0100

    feat(models): add Company and Expense models, update User model with relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Enums\Role;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -18,6 +22,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'company_id',
+        'role',
         'name',
         'email',
         'password',
@@ -41,5 +47,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'role' => Role::class,
     ];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function expenses(): HasMany
+    {
+        return $this->hasMany(Expense::class);
+    }
 }
---
+++ database/migrations/2025_04_16_091215_add_company_id_and_role_to_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('company_id')
+                ->constrained('companies')
+                ->onDelete('cascade')
+                ->after('id');
+            $table->string('role')->after('company_id')->index();
+
+            // Since I'm using mysql, I dont need to add the index manually as the
+            // foreign key will automatically create an index for the foreign key column.
+            // $table->index('company_id');
+
+            $table->index(['company_id', 'role'], 'company_role_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['company_id']);
+            $table->dropIndex('company_role_index');
+            $table->dropColumn([
+                'company_id',
+                'role',
+            ]);
+        });
+    }
+};
