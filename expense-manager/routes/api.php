8a5f1f87389421e5d382d9b8804ce16c5653f4fb
Ade-ojo Toluwanimi <59847687+ManLikeTeetos@users.noreply.github.com>
2025-04-17 00:16:05 +0000

    Database seeder for Admin to Test Auth Api

--- expense-manager/routes/api.php
+++ expense-manager/routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AuthController;
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    //Admin on register
+    //Admin on registercd 
     Route::post('/register', [AuthController::class, 'register'])->middleware('role:Admin');
     Route::post('/logout', [AuthController::class, 'logout']);
 });
