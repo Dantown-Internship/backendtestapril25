74efd8aeb0ae035f54fc75815d8a6342ccfd8e64
EfiokobongN <mailpreciousnelson@gmail.com>
2025-04-18 10:32:33 +0000

    task four done

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -7,30 +7,39 @@ use App\Http\Requests\ExpensePostRequest;
 use App\Models\Expense;
 use App\utility\Util;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class ExpenseController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $user = Util::Auth();
-        $query = Expense::with('user')->where('company_id', $user->company_id);
-        if ($search = $request->query('search')) {
-            $query->where(function ($q) use ($search) {
-                $q->where('title', 'like', "%search%")
-                ->orWhere('category', 'like', "%search%");
-            });
-        }
-        return response()->json($query->paginate(10));
+
+        $cacheKey = "expenses_{$user->company_id}_page_" . $request->get('page', 1) . '_search_' . $request->get('search');
+        $expenses = Cache::remember($cacheKey, 60, function () use ($user, $request) {
+            $query = Expense::with('user')->where('company_id', $user->company_id);
+            if ($search = $request->query('search')) {
+                $query->where(function ($q) use ($search) {
+                    $q->where('title', 'like', "%search%")
+                        ->orWhere('category', 'like', "%search%");
+                });
+            }
+            return $query->paginate(10);
+        });
+        return response()->json($expenses);
     }
 
 
-    public function create(ExpensePostRequest $request){
+    public function create(ExpensePostRequest $request)
+    {
         $this->authorize('create', Expense::class);
         try {
             $user = Util::Auth();
             //Admin Create A New Expense
             $expense = Expense::createExpenseRecord($request, $user->id, $user->company_id);
+            // clear first pegination
+            Cache::forget("expenses_{$user->company_id}_page_1_search_");
             return response()->json(['sucess' => true, 'message' => 'Expense Added Successfully', 'expense' => $expense]);
-
         } catch (\Throwable $th) {
             return response()->json(['sucess' => false, 'message' => $th->getMessage()]);
         }
@@ -38,31 +47,35 @@ class ExpenseController extends Controller
 
 
 
-    public function update(ExpenseEditRequest $request, Expense $expense){
+    public function update(ExpenseEditRequest $request, Expense $expense)
+    {
         $this->authorize('update', $expense);
 
-       try {
-        $expense->title = $request->title ?? $expense->title;
-        $expense->category = $request->category ?? $expense->category;
-        $expense->amount = $request->amount ?? $expense->amount;
+        try {
+            $expense->title = $request->title ?? $expense->title;
+            $expense->category = $request->category ?? $expense->category;
+            $expense->amount = $request->amount ?? $expense->amount;
+            $expense->update();
 
-        $expense->update();
-
-        return response()->json(['success' => true, 'message' => 'Expense Updated Successfully', 'expense' => $expense]);
-       } catch (\Throwable $th) {
-        return response()->json(['sucess' => false, 'message' => $th->getMessage()]);
-       }
+            // clear first pegination
+            Cache::forget("expenses_{$expense->company_id}_page_1_search_");
+            return response()->json(['success' => true, 'message' => 'Expense Updated Successfully', 'expense' => $expense]);
+        } catch (\Throwable $th) {
+            return response()->json(['sucess' => false, 'message' => $th->getMessage()]);
+        }
     }
 
-    public function delete(Expense $expense){
+    public function delete(Expense $expense)
+    {
         $this->authorize('delete', $expense);
 
-       try {
-        $expense->delete();
-
-        return response()->json(['success' => true, 'message' => 'Expense Deleted Successfully']);
-       } catch (\Throwable $th) {
-        return response()->json(['sucess' => false, 'message' => $th->getMessage()]);
-       }
+        try {
+            $expense->delete();
+            // clear first pegination
+            Cache::forget("expenses_{$expense->company_id}_page_1_search_");
+            return response()->json(['success' => true, 'message' => 'Expense Deleted Successfully']);
+        } catch (\Throwable $th) {
+            return response()->json(['sucess' => false, 'message' => $th->getMessage()]);
+        }
     }
 }
