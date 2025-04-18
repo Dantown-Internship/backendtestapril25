5f9e0b338c16cad010cba4a4b6096641d3b54a22
JohnChidozie <johnchidozie320@gmail.com>
2025-04-18 07:02:01 -0700

    Completed Task 6  which is to create an Observer That will Log Updates and Delete and store old and new values of each expense

--- app/Models/Audit_Log.php
+++ app/Models/Audit_Log.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Audit_Log extends Model
 {
-    //
+    protected $table= "audit_logs";
+    protected $casts = [
+        'changes' => 'array',
+    ];
+
+    protected $fillable = ['user_id', 'company_id', 'action', 'changes'];
+
+    
 }
+
+
