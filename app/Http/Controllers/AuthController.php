5b53355a76d9411d690e30212d32636c98eb7a1e
Kingsley Udenewu <kingzpacking@gmail.com>
2025-04-18 10:49:02 +0100

    Generate audit log and factor classes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Actions\GenerateTokenAction;
+use App\Actions\RegisterAction;
+use App\Data\LoginData;
+use App\Data\RegisterData;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(RegisterData $data): JsonResponse
+    {
+        $user = app(RegisterAction::class)->execute($data);
+
+        $tokenPayload = (new GenerateTokenAction())->execute($user);
+
+        return new JsonResponse(
+            [
+                'message' => 'User Registered Successfully',
+                'data' => $tokenPayload,
+            ],
+            Response::HTTP_CREATED
+        );
+    }
+
+    public function login(LoginData $data): JsonResponse
+    {
+        $tokenPayload = (new GenerateTokenAction())->execute($data->user);
+
+        return new JsonResponse(
+            [
+                'message' => 'User Logged In Successfully',
+                'data' => $tokenPayload,
+            ],
+            Response::HTTP_OK
+        );
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return new JsonResponse(
+            [
+                'message' => 'User Logged Out Successfully',
+            ],
+            Response::HTTP_OK
+        );
+    }
+}
--- app/Models/Company.php
+++ app/Models/Company.php
@@ -9,4 +9,16 @@ class Company extends Model
 {
     /** @use HasFactory<\Database\Factories\CompanyFactory> */
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function users(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function expenses(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Expense::class);
+    }
 }
