1c2cbbd77a9c3c15f1d43baa5cd60db8b6efab57
EfiokobongN <mailpreciousnelson@gmail.com>
2025-04-18 21:01:18 +0000

    correction

--- app/Mail/WeeklyExpenseReport.php
+++ app/Mail/WeeklyExpenseReport.php
@@ -3,10 +3,9 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
+
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
+
 use Illuminate\Queue\SerializesModels;
 
 class WeeklyExpenseReport extends Mailable
