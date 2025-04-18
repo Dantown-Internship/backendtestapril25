afd4241db863ea307e8ff8bf96ae7ee4f41e452f
Osememen Ogbe <osememen.ogbe@gmail.com>
2025-04-18 17:47:14 +0100

    user factory company_id set at random

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'company_id' => Company::factory(),
+            'company_id' => Company::inRandomOrder()->first()->id,
             'role' => 'Admin',
         ];
     }
