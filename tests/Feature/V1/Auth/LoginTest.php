3b32114f5720d756957cbc7634e5e1a11408c6f6
Favour Oladeji <oladejifavour100@gmail.com>
2025-04-18 09:10:43 +0100

    refactor: remove repeated test

--- tests/Feature/V1/Auth/LoginTest.php
+++ tests/Feature/V1/Auth/LoginTest.php
@@ -40,24 +40,3 @@ test('users can not authenticate with invalid password', function () {
         'status' => false
     ])->assertStatus(422);
 });
-
-test("user's token gets deleted on logout", function () {
-    $user = User::factory()->create();
-
-    $token = $user->createToken('test-token')->plainTextToken;
-    // Make the request with the token
-    $response = $this->withHeaders([
-        'Authorization' => 'Bearer ' . $token,
-    ])->postJson(route('api.v1.logout'));
-
-    // Assert the response
-    $response
-        ->assertStatus(200)
-        ->assertJson([
-            'status' => true,
-            'message' => 'Logout successful',
-        ]);
-
-    // Assert the token was deleted
-    expect($user->tokens->count())->toBe(0);
-});
